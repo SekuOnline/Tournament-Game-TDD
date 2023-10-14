93ea8146a5079f9a52fc1996fad7e992c9124fd9
David <davidsedarous@cmail.cartleon.ca>
2023-10-14 14:26:57 -0400

    CODE 010: Implemented check to mark poisoned cards as poisoned.
    Test passes.

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,12 +1,16 @@
 package org.example;
 
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Card {
 
-
+    private static List<Integer> swP = Arrays.asList(6, 7, 8, 9);
+    private static List<Integer> arP = Arrays.asList(8,9,10,11);
+    private static List<Integer> soP = Arrays.asList(5,6,11,12);
+    private static List<Integer> deP = Arrays.asList(6, 7, 8, 9);
     //private variables
     Suit suit;
     boolean poisoned;
@@ -15,7 +19,22 @@ public class Card {
     int value;
 
     //Constructor
-    public Card(Suit suit, int value){this.suit = suit; this.value = value;}
+    public Card(Suit suit, int value){
+        this.suit = suit;
+        this.value = value;
+        switch(suit){
+            case SW:
+                if (swP.contains(value)){this.poisoned = true;} break;
+            case AR:
+                if (arP.contains(value)){this.poisoned = true;} break;
+            case SO:
+                if (soP.contains(value)){this.poisoned = true;} break;
+            case DE:
+                if (swP.contains(value)){this.poisoned = true;} break;
+
+            default: this.poisoned = false;
+        }
+    }
 
     //getters
     public Suit getSuit(){return suit;}
